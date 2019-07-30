58e7a6156391dab016af8edd440e7826313afc6d
xljiulang <366193849@qq.com>
2019-07-30 20:18:58 +0800

    增加Benchmark

---
+++ MiniMapper.Benchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace MiniMapper.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<MapContext>();
+            Console.ReadLine();
+        }
+    }
+}

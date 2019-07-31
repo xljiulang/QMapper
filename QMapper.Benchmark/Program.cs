11df6a1d626f30de3a1236172debc157d61092d7
xljiulang <366193849@qq.com>
2019-07-31 10:17:20 +0800

    换项目名

---
+++ QMapper.Benchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace QMapper.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<MapContext>();
+            Console.ReadLine();
+        }
+    }
+}
